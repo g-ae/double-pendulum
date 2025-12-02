ce6dbc02ff32182040cc9e34c59c69314bf6c33f
g-ae <goncalo.arieira@hotmail.com>
2025-12-02 12:33:29 +0100

    modification de paramètres

--- basicpendulum.jl
+++ basicpendulum.jl
@@ -5,12 +5,12 @@ using Plots
 # Constants
 const l = 5
 const g = 9.81
-const delta_t = 0.001
-const iterations = 5000
+const delta_t = 0.01
+const iterations = 800
 
 # Base values
-theta = 1.0
-thetap = 0.0 # base speed
+theta = 3.1415
+thetap = 0.6 # base speed
 
 # Position : (0,0) = base
 x = [sin(theta) * l]
@@ -36,9 +36,13 @@ for i in 1:iterations
     push!(y, -l * cos(theta))
 end
 
-anim = @animate for i in 1:20:length(x)
-	Plots.plot(x[1:i], y[1:i], aspect_ratio = :equal)
+anim = @animate for i in 1:length(x)
+    if i % 100 == 0
+        println("Progression ", i, "/", length(x))
+    end
+	Plots.plot(x[1:i], y[1:i], aspect_ratio = :equal, size=(600, 600), axis=([], false))
+	Plots.plot!([0, x[i]], [0, y[i]], color=:black)
     Plots.scatter!(x[i:i], y[i:i], xlims=(-6,6), ylims=(-6,6), marker=true, color=:red, legend=false)
 end
 
-gif(anim, "basicpendulum.gif", fps = 30)
+gif(anim, "basicpendulum.gif", fps = 100)
